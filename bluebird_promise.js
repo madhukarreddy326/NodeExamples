f21495c0031cfa7dcfdba564e6b23042afd4e1bc
Madhukar <madhukar.reddy326@gmail.com>
2020-06-07 18:02:42 +0530

    bluebird_promise example

---
+++ bluebird_promise.js
@@ -0,0 +1,12 @@
+var Promise = require('bluebird');
+var mongoClient = Promise.promisifyAll(require('mpngodb')).MongoClient;
+var url ='mongodb://localhost/EmployeeDB';
+mongoClient.connectAsync('mongodb://localhost/EmployeeDB')
+.then(function(db){
+    return db.collection('Employee').findAsync({})
+})
+.then(function(cursor){
+    cursor.each(function(err,doc){
+        console.log(doc);
+    })
+})
