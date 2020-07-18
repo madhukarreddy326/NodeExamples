27f5af161afcf2aea66f6c86cec272b95ab2d4bf
Madhukar <madhukar.reddy326@gmail.com>
2020-07-18 12:55:08 +0530

    environment specific port reading and assiging the same

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to express world");
+    res.send("Welcome to express world  !!!");
 
 });
 
@@ -10,6 +10,7 @@ app.get('/api/courses',(req,res) =>{
     res.send([1,2,3]);
 
 });
+const port =process.env.PORT ||3000;
 
 
-app.listen(3000,()=> console.log("server started"));
+app.listen(port,()=> console.log(`started the server and listening on ${port}`));
