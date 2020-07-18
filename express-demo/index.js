10b5b4e418bb89aca0bee254b7dd2fc6d7ea8662
Madhukar <madhukar.reddy326@gmail.com>
2020-07-18 13:04:35 +0530

    get path parameter example

--- express-demo/index.js
+++ express-demo/index.js
@@ -10,6 +10,13 @@ app.get('/api/courses',(req,res) =>{
     res.send([1,2,3]);
 
 });
+app.get('/api/courses/:id',(req,res)=>
+{
+    res.send(req.params.id);
+   // res.send(req.params);//this will list all the path parameters
+    //res.send(req.query);//this will list all the query parameters
+})
+
 const port =process.env.PORT ||3000;
 
 
