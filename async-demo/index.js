56b4bb6ad6c0330f2df574819dfaf464bbf25291
Madhukar <madhukar.reddy326@gmail.com>
2020-07-18 22:56:30 +0530

    async exmpales

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,23 @@
 console.log("starting");
-const result = getUser(1);
-console.log(result);
+getUser(1,(user)=>{
+    console.log('User:',user);
+});
+getRespositories('madhukar',(list)=>{
+    console.log('List ::',list);
+});
 console.log("ending");
 
-function getUser(id)
+function getUser(id,callback)
 {    setTimeout(() =>{
         console.log("Reading data from the data bases.....");
-        return {id:id,name:'madhukar'};
+        callback({id:id,name:'madhukar'});
         },2000);
+}
+
+function getRespositories(userName,callback)
+{
+    setTimeout(() =>{
+        callback( ['rep1','repo2','repo3']);
+    },2000);
+    
 }
